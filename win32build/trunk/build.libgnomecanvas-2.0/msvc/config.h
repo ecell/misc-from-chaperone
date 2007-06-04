0580377df94ce4cdc0fd72c3d6bbd9c91468f20c
moriyoshi <moriyoshi@896f1ba6-c1ac-4286-9cf6-b32da33d4692>
2007-06-04 09:43:12 +0000

    * Initial.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/misc@1 896f1ba6-c1ac-4286-9cf6-b32da33d4692

---
+++ win32build/trunk/build.libgnomecanvas-2.0/msvc/config.h
@@ -0,0 +1,79 @@
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#undef ENABLE_NLS
+
+/* Gettext package */
+#undef GETTEXT_PACKAGE
+
+/* Define the location where the catalogs will be installed */
+#undef GNOMECANVASLOCALEDIR
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#undef HAVE_BIND_TEXTDOMAIN_CODESET
+
+/* Define to 1 if you have the `dcgettext' function. */
+#undef HAVE_DCGETTEXT
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#undef HAVE_DLFCN_H
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#undef HAVE_GETTEXT
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#undef HAVE_LC_MESSAGES
+
+/* Define to 1 if you have the <locale.h> header file. */
+#undef HAVE_LOCALE_H
+
+/* Define to 1 if you have the <memory.h> header file. */
+#undef HAVE_MEMORY_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#undef HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "libgnomecanvas"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://bugzilla.gnome.org/enter_bug.cgi?product=libgnomecanvas"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libgnomecanvas"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libgnomecanvas 2.14.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libgnomecanvas"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.14.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "2.14.0"
